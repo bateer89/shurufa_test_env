773be7fe31d8e25903295296db1cff135b20250f
bateer89 <wdyxbte@163.com>
2021-01-08 10:59:17 +0800

    Update IZnUserWordsService.java

--- jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo3/words/service/IZnUserWordsService.java
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo3/words/service/IZnUserWordsService.java
@@ -3,12 +3,10 @@ package org.jeecg.modules.demo3.words.service;
 import org.jeecg.modules.demo3.words.entity.ZnUserWords;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-import java.util.List;
-
 /**
- * @Description: 用户汉字完成表
+ * @Description: 用户任务表
  * @Author: jeecg-boot
- * @Date:   2021-01-06
+ * @Date:   2021-01-08
  * @Version: V1.0
  */
 public interface IZnUserWordsService extends IService<ZnUserWords> {
